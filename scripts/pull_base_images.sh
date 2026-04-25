23a3118ed62890b511781324e6c10c625a15c2c2
amenallah-salem <salem.amenalah@gmail.com>
2026-04-25 12:58:41 +0200

    add sh file to pull images for faster build

---
+++ scripts/pull_base_images.sh
@@ -0,0 +1,6 @@
+docker pull python:3.12-slim
+docker pull python:3.11-slim-bookworm
+docker pull node:20-alpine
+docker pull nginx:alpine
+docker pull pgvector/pgvector:pg16
+docker pull n8nio/n8n:latest
